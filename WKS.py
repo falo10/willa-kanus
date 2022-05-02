a95d8c580767f3cf737dc74629869e648774bb48
falo10 <swiecinskiolaf@gmail.com>
2022-05-02 19:59:08 +0200

    change of renaming of other objects

--- WKS.py
+++ WKS.py
@@ -19,65 +19,65 @@ money = [0, 200, 800, 2000]
 
 Person = IntEnum ('Osoba', 'Bambus Falo Artur')
 
-osobaSzansa ={Person.Bambus: 0.2,
-              Person.Falo: 0.7,
-              Person.Artur: 0.1}
+chanceForPerson ={Person.Bambus: 0.2,
+                  Person.Falo: 0.7,
+                  Person.Artur: 0.1}
 
-osobyLista = list (osobaSzansa.keys())
+persons = list (chanceForPerson.keys())
 
-sznasaLista = list (osobaSzansa.values())
-
-
-
-ListaArtefaktow = IntEnum ('artefakty', ["Złamany_szlug", "Kij_golfowy", "Flaszka", "Joint", "PIWKO_330"])
-
-Artefakty = list(ListaArtefaktow)
-
-def aproksymacja(nagroda):
-    przychod = round (uniform (nagroda - 0.1*nagroda, nagroda + 0.1))
-    return przychod
+chances = list (chanceForPerson.values())
 
 
-def nagroda():
+
+ArtefactsNames = IntEnum ('artefakty', ["Złamany_szlug", "Kij_golfowy", "Flaszka", "Joint", "PIWKO_330"])
+
+Artefacts = list(ArtefactsNames)
+
+def approximation(capture):
+    income = round (uniform (capture - 0.1*capture, capture + 0.1))
+    return income
+
+
+def award():
     if (luck == 1):
-        zdobytyArtefakt =  choices(Artefakty, [30, 25, 20, 15, 5], k=1)[0]
+        gainedArtefact =  choices(Artefacts, [30, 25, 20, 15, 5], k=1)[0]
     elif (luck == 2):
-        zdobytyArtefakt =  choices(Artefakty, [15, 25, 20, 15, 10], k=1)[0]
+        gainedArtefact =  choices(Artefacts, [15, 25, 20, 15, 10], k=1)[0]
 
-    if (zdobytyArtefakt == ListaArtefaktow.Złamany_szlug):
-        print ('Ehhhh pierdolone zwierzęta .... miały być artefakty a jedyne co znalazłeś to:', ListaArtefaktow.Złamany_szlug.name, ', który jest nic nie warty')
+    if (gainedArtefact == ArtefactsNames.Złamany_szlug):
+        print ('Ehhhh pierdolone zwierzęta .... miały być artefakty a jedyne co znalazłeś to:', ArtefactsNames.Złamany_szlug.name, ', który jest nic nie warty')
         return 0
-    elif (zdobytyArtefakt == ListaArtefaktow.Kij_golfowy):
-        print (ListaArtefaktow.Kij_golfowy.name, '! To może być coś warte!')
-        wartosc = aproksymacja(100)
+    elif (gainedArtefact == ArtefactsNames.Kij_golfowy):
+        print (ArtefactsNames.Kij_golfowy.name, '! To może być coś warte!')
+        wartosc = approximation(100)
         print (f"""
 
                 Otrzymujesz {wartosc} zł
 
                     """)
         return wartosc
-    elif (zdobytyArtefakt == ListaArtefaktow.Joint):
-        print (ListaArtefaktow.Joint.name, '! Sprzedam go Bambusowi!')
-        wartosc = aproksymacja(300)
+    elif (gainedArtefact == ArtefactsNames.Joint):
+        print (ArtefactsNames.Joint.name, '! Sprzedam go Bambusowi!')
+        wartosc = approximation(300)
         print (f"""
 
                 Otrzymujesz {wartosc} zł
 
                 """)
         return wartosc
-    elif (zdobytyArtefakt == ListaArtefaktow.Flaszka):
-        print (ListaArtefaktow.Flaszka, '! To na pewno jest coś warte!')
-        wartosc = aproksymacja(200)
+    elif (gainedArtefact == ArtefactsNames.Flaszka):
+        print (ArtefactsNames.Flaszka, '! To na pewno jest coś warte!')
+        wartosc = approximation(200)
         print (f"""
 
                 Otrzymujesz {wartosc} zł
 
                 """)
         return wartosc
-    elif (zdobytyArtefakt == ListaArtefaktow.PIWKO_330):
+    elif (gainedArtefact == ArtefactsNames.PIWKO_330):
         print ("NIE WIERZĘ WŁASNYM OCZOM!!!")
-        print (ListaArtefaktow.PIWKO_330.name)
-        wartosc = aproksymacja(700)
+        print (ArtefactsNames.PIWKO_330.name)
+        wartosc = approximation(700)
         print (f"""
 
                 Otrzymujesz {wartosc} zł
@@ -113,9 +113,9 @@ Ci któzy przetrwali, napisali legendę!
 
 
 
-imie = input('Podaj Twoję imię przywoływaczu: ')
+name = input('Podaj Twoję imię przywoływaczu: ')
 
-print('\nWitaj', imie, 'sprawdźmy czy jesteś gotowy na wyjście cało z tej mistycznej posiadłości \n')
+print('\nWitaj', name, 'sprawdźmy czy jesteś gotowy na wyjście cało z tej mistycznej posiadłości \n')
 
 
 print("""
@@ -128,45 +128,45 @@ Czas na wybranie klasy postaci! Każda klasa cechuje się inną specjalną umiej
 
 
 while True:
-    wybor = input('Jaki jest Twój wybór? Napisz klasę postaci, którą chcesz wybrać!: ')
-    if (wybor == characterClass.Banan.value):
-        otrzymanaKaska = choices(money,[0.15, 0.20, 0.55, 0.10], k=1)[0]
-        if (otrzymanaKaska == 0):
+    decision = input('Jaki jest Twój wybór? Napisz klasę postaci, którą chcesz wybrać!: ')
+    if (decision == characterClass.Banan.value):
+        receivedMoney = choices(money,[0.15, 0.20, 0.55, 0.10], k=1)[0]
+        if (receivedMoney == 0):
             print('Jesteś takim giga grzybem, że stary nie zostawiłby Ci nawet złamanego pensa! radź sobie sam xd')
-        elif (otrzymanaKaska == 200):
+        elif (receivedMoney == 200):
             print ('Masz tu 2 stówy, nie dzwoń o więcej!')
-            bag += otrzymanaKaska
-        elif (otrzymanaKaska == 800):
+            bag += receivedMoney
+        elif (receivedMoney == 800):
             print ('Nie po to tatuś się tyle namęczył, żeby jego potomek nie potrafił przejść przez jakąś letniskową chatke! Masz tutaj 800 zł ode mnie!')
-            bag += otrzymanaKaska
-        elif (otrzymanaKaska == 2000):
+            bag += receivedMoney
+        elif (receivedMoney == 2000):
             print ('Dla mojego ukochanego wszystko. Myślę że 2000 zamkną wszystki usta!')
-            bag+= otrzymanaKaska
+            bag+= receivedMoney
         break
-    elif (wybor == characterClass.Pytalski.value):
-        kto = choices (osobyLista, sznasaLista, k=1)[0]
-        if (kto == Person.Bambus):
+    elif (decision == characterClass.Pytalski.value):
+        friend = choices (persons, chances, k=1)[0]
+        if (friend == Person.Bambus):
             print (f"""<widzisz Bambusa z czerwonymi oczami, jakby przez godzine pływał na basenie bez okularów>
-OOOO siema {imie}, jasne byczq już Ci mówię trasę... To było tak:
+OOOO siema {name}, jasne byczq już Ci mówię trasę... To było tak:
 {road[0]}
-kurwa sorry {imie} ,ale w chuj się zjarałem i nie pamiętam dalej... powodzenia xd""")
-        elif (kto == Person.Falo):
+kurwa sorry {name} ,ale w chuj się zjarałem i nie pamiętam dalej... powodzenia xd""")
+        elif (friend == Person.Falo):
             print (f""" <Słyszysz szmer drapania się po skórze, domyślasz sie, że poblizu musi byc gdzieś Falo!>
-{imie}!! Co tam morda? Pewnie że pamiętam trasę...To było:
+{name}!! Co tam morda? Pewnie że pamiętam trasę...To było:
 {road[0]}
 {road[1]}
 {road[2]}
 kurwa pamięć jak zwykle zawodzi xd ale i tak dużo powiedziałem to chyba dasz radę""")
-        elif (kto == Person.Artur):
+        elif (friend == Person.Artur):
             print(f"""<przed drzwiami widisz Artura którego cała twarz jest czerwona od słońca.>
-O cześć {imie}. Jasne, że znam trasę! To bedzie:
+O cześć {name}. Jasne, że znam trasę! To bedzie:
 {road[0]}
 {road[1]}
 {road[2]}
 {road[3]}
 ostatniego Ci nie powiem bo byo za łatwo xd""")
         break
-    elif (wybor == characterClass.Farciarz.value):
+    elif (decision == characterClass.Farciarz.value):
         luck = 2
         print ('Wiesz co mówią o tych co liczą na szczęście?!')
         break
@@ -230,13 +230,13 @@ if (bag > 0):
 
     while True:
 
-        PierwszyKrok = input ("Jaki jest Twój ruch: ")
+        firstStep = input ("Jaki jest Twój ruch: ")
 
 
-        if (PierwszyKrok == road[0]):
+        if (firstStep == road[0]):
             print ('Pierwszy krok za Tobą! Chwała co raz bliżej')
             print ('W związku ze wskazaniem dobrej drogi otrzymujesz: ')
-            bonus = nagroda()
+            bonus = award()
             bag += bonus
             print ('Zawartość Twojego portfela to:', bag)
             print ("""
@@ -251,7 +251,7 @@ if (bag > 0):
 """)
             break
 
-        elif (PierwszyKrok != road[0]):
+        elif (firstStep != road[0]):
 
             bag -= 100
             if (bag <0):
@@ -279,19 +279,19 @@ NIE KAŻDY MOŻE ZOSTAĆ KOLEJNĄ LEGENDĄ""")
 
 
 
-for z in range (1,4):
+for step in range (1,4):
 
     if (bag >= 0):
 
         while True:
 
-            ruch = input ("Jaki jest Twój kolejny ruch: ")
+            move = input ("Jaki jest Twój kolejny ruch: ")
 
 
-            if (ruch == road[z]):
+            if (move == road[step]):
                 print ('Chwała co raz bliżej')
                 print (' W związku ze wskazaniem dobrej drogi otrzymujesz: ')
-                bonus = nagroda()
+                bonus = award()
                 bag += bonus
                 print ('Zawartość Twojego portfela to:', bag)
                 print ("""
@@ -303,7 +303,7 @@ Zblizasz się do celu!
 
 """)
                 break
-            elif (ruch != road[z]):
+            elif (move != road[step]):
 
                 bag -= 100
                 if (bag <0):
@@ -342,10 +342,10 @@ ALBO NIGDY.....
 
     while True:
 
-            ruch = input ("Czy kolejny ruch pozwoli Ci zostać kimś, kim zawsze chciałeś się stać? Jaki jest Twój kolejny krok?  ")
+            move = input ("Czy kolejny ruch pozwoli Ci zostać kimś, kim zawsze chciałeś się stać? Jaki jest Twój kolejny krok?  ")
 
 
-            if (ruch == road[4]):
+            if (move == road[4]):
                 print (f"""
 
 
@@ -353,9 +353,9 @@ ALBO NIGDY.....
 
     KAMIL TO WCIAŻ PIZDA
 
-    A ty {imie}, {imie} JESTEŚ KOTEM""")
+    A ty {name}, {name} JESTEŚ KOTEM""")
                 break
-            elif (ruch != road[4]):
+            elif (move != road[4]):
 
                 bag -= 100
                 if (bag <0):
